4f370e18f26261aaf80647a1ced036d2acc4c44b
Verónica Moreno <veronicamorenoalgarin@outlook.es>
2024-05-01 12:21:43 +0200

    some bugs fixed. 12.21

--- ProyectoSonrisas/Assets/LevelSelector.cs
+++ ProyectoSonrisas/Assets/LevelSelector.cs
@@ -3,18 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 using UnityEngine.XR.Interaction;
 using UnityEngine.XR.Interaction.Toolkit.Inputs;
 
 public class LevelSelector : MonoBehaviour
 {
     private FollowPathOutside followPath;
-
+    [SerializeField] private GameObject pointer;
     public InputActionReference trigger;
     public bool isTouchingBook= false;
+    private ChestManager panel;
+    private bool isTriggerPressed = false;
     private void Start()
     {
         followPath= FindObjectOfType<FollowPathOutside>();
+         panel=FindObjectOfType<ChestManager>();
     }
     private void Update()
     {
@@ -30,8 +34,13 @@ public class LevelSelector : MonoBehaviour
     public void GoToLevelOne()
     {
         float triggerValue = trigger.action.ReadValue<float>();
-        if (triggerValue>0 )
-        {   
+        if (triggerValue>0 && !isTriggerPressed)
+        {
+            isTriggerPressed = true;
+            //desactivar el pointer de la camara
+            pointer.SetActive(false);
+            panel.hideTooltip();
+            
             //deberia salir panel de confirmación para saber que es el libro que quiere
             //y una vez confirmado el libro de ancla a la mano
             StartCoroutine(followPath.MoveToTargets());
